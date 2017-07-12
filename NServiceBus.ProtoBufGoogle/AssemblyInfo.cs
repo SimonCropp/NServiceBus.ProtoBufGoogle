a25ba3415bac5c76c347f11acf953e75c4a6f222
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 13:31:51 +1000

    simplify build

--- NServiceBus.ProtoBufGoogle/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NServiceBus.ProtoBufGoogle")]
-[assembly: AssemblyProduct("NServiceBus.ProtoBufGoogle")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
