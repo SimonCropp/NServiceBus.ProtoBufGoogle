073a77221bb1eb41adc47a23518927e39d87fd8c
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 11:55:33 +1000

    net452

--- NServiceBus.ProtoBufGoogle/AssemblyInfo.cs
+++ NServiceBus.ProtoBufGoogle/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("NServiceBus.ProtoBufGoogle")]
 [assembly: AssemblyProduct("NServiceBus.ProtoBufGoogle")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
